0bd8ecc396209a8df6d1370841f8285a78390b69
sonphuong <dosonphuong@gmail.com>
2017-05-11 18:35:05 -0500

    php-test: add comments

--- php-test/index.php
+++ php-test/index.php
@@ -18,27 +18,32 @@
     </header>
     <div class="wrapper">
       <?php
+      //get date and time of now
       $now = strtotime("now");
+      //initiate 2 next times
       $nextTime='';
       $suppliedTime='';
-
+      //2 possiblities are this week or next week
       $thisWed = strtotime("Wednesday this week 8pm");
       $thisSat = strtotime("Saturday this week 8pm");
       $nextWed = strtotime("next Wednesday 8pm");
       $nextSat  = strtotime("next Saturday 8pm");
-      //case 1 compare now to this Wednesday
+      //case 1 compare now with this Wednesday
       if($now<$thisWed){
         $nextTime = $thisWed;
         $suppliedTime = $thisSat;
       }
+      //case 2 between Wed & Sat
       elseif($now>$thisWed && $now<$thisSat){
         $nextTime = $thisSat;
         $suppliedTime = $nextWed;
       }
+      //case 3 bigger than this Saturday => take next week value
       elseif($now>$thisSat){
         $nextTime = $nextWed;
         $suppliedTime = $nextSat;
       }
+      //give more infor if we can bet today
       $moreInfo = "";
       $today = date("Y-m-d", $now);
       $nextTimeDay = date("Y-m-d", $nextTime);
@@ -49,11 +54,7 @@
       ?>
       <h4>Next Time: <?php echo $moreInfo.date( "ha, l, Y-M-d", $nextTime); ?></h4>
       <h4>Supplied Time: <?php echo date( "ha, l, Y-M-d", $suppliedTime); ?></h4>
-      <?php
 
-
-
-      ?>
     </div>
   </section>
 </body>
