cf4beb7945bbed3eaeedf5d80aaf1f93cc71c3de
sonphuong <dosonphuong@gmail.com>
2017-05-12 11:59:01 -0500

    for all device

--- html-test/main.js
+++ html-test/main.js
@@ -1,14 +1,19 @@
 (function() {
   function flip(el) {
     var obj = document.getElementById('card');
+    var objTscsText =  document.getElementById('tscs-text');
     if (obj.getAttribute('flipped') === 'true') {
       obj.children[1].style.transform = "perspective(600px) rotateY(-180deg)";
       obj.children[0].style.transform = "perspective(600px) rotateY(0deg)";
       obj.setAttribute('flipped', 'false');
+      objTscsText.classList.remove('overflow-y')
+      console.log('front');
     } else {
       obj.children[1].style.transform = "perspective(600px) rotateY(0deg)";
       obj.children[0].style.transform = "perspective(600px) rotateY(180deg)";
       obj.setAttribute('flipped', 'true');
+      setTimeout(function(){ objTscsText.classList.add('overflow-y'); }, 2);
+      console.log('back');
     }
   }
 
